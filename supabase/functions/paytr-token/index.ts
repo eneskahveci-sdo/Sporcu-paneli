db544440604731125b3f73888efc0b0390274ba5
eneskahveci-sdo <muhammeteneskahveci@gmail.com>
2026-03-16 20:52:39 +0300

    Move paytr-token function to new directory

--- supabase/functions/paytr-token/index.ts
+++ supabase/functions/paytr-token/index.ts
@@ -4,6 +4,7 @@ import { createHmac } from "https://deno.land/std@0.177.0/node/crypto.ts";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
 
